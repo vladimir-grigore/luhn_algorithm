610a1b973951d646aa3bbb15cc1449c24b3af6dd
vagrant <vladimir.grigore@gmail.com>
2017-03-10 23:23:54 +0000

    Compute array for every other digit

--- check.js
+++ check.js
@@ -1,11 +1,32 @@
 function check(number) {
   //convert number into string and split it into an array
   var num = ("" + number).split('');
+  var computedArray = computeCheckDigits(num);
 
+}
+
+function computeCheckDigits(array) {
+  var resultArray = [];
+  var is_check_digit = false;
   //traverse array from right to left
-  for (var i = num.length - 1; i >= 0; i--) {
-    console.log(num[i])
+  for (var i = array.length - 1; i >= 0; i--) {
+    //compute every other digit
+    if (is_check_digit) {
+      //multiply number by 2 and check if it's bigger than 9
+      var multiply_by_two = array[i] * 2;
+      if (multiply_by_two > 9) {
+        multiply_by_two -= 9;
+      }
+      resultArray[i] = multiply_by_two;
+      is_check_digit = false;
+    } else {
+      resultArray[i] = array[i];
+      is_check_digit = true;
+    }
+
   }
+
+  return resultArray;
 }
 
 check(79927398713)
